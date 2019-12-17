ea74dc3bea7a4dc6019c0fb4ecb635d3093b0176
Samuel Shepard <vfn4@cdc.gov>
2019-12-17 12:31:48 -0500

    Initial effort at the mathutils library.

---
+++ udf-mathutils.cc
@@ -0,0 +1,78 @@
+// udf-bioutils.cc - Sam Shepard - 2019
+// Impala user-defined functions for mathematical utilities.
+
+#include "udf-mathutils.h"
+#include "common.h"
+
+#include <boost/math/distributions/students_t.hpp>
+#include <boost/math/distributions/normal.hpp>
+using namespace boost::math;
+using namespace std;   
+
+// Quantile function for t-statistic generation
+IMPALA_UDF_EXPORT
+DoubleVal qt(FunctionContext* context, const DoubleVal& confidence, const BigIntVal& sample_size, const BooleanVal& two_tailed ) {
+	if ( confidence.is_null || two_tailed.is_null || sample_size.is_null ) { return DoubleVal::null(); }
+	if ( confidence.val > 1 || confidence.val < 0 || sample_size.val < 2 ) { return DoubleVal::null(); }
+
+  	int64_t Sn = sample_size.val;
+	double T = 0;
+
+	// Rounding errors occur past this point. Since the T distribution converges toward the normal distribution as N grows large,
+	// we can switch to using the normal distribution without much of a difference in the statistic.
+	if ( Sn > 1404454273 ) {
+		normal dist(0.0,1.0);
+		if( two_tailed.val ) {
+			T = quantile( complement(dist, (1-confidence.val)/2.0 ) );
+		} else {
+			T = quantile( dist, confidence.val );
+		}
+	} else {
+		students_t dist(Sn - 1);
+		if( two_tailed.val ) {
+			T = quantile( complement(dist, (1-confidence.val)/2.0 ) );
+		} else {
+			T = quantile( dist, confidence.val );
+		}
+	}
+
+	return DoubleVal(T);
+}
+
+// Confidence interval function. Normally we take alpha but I think backwards, so we take the confidence level instead.
+IMPALA_UDF_EXPORT
+DoubleVal ci_t(FunctionContext* context, const DoubleVal& confidence, const BigIntVal& sample_size, const DoubleVal& sample_std, const BooleanVal& two_tailed ) {
+	if ( confidence.is_null || two_tailed.is_null || sample_size.is_null || sample_std.is_null ) { return DoubleVal::null(); }
+	if ( confidence.val > 1 || confidence.val < 0 || sample_size.val < 2 || sample_std.val < 0 ) { return DoubleVal::null(); }
+
+  	int64_t Sn = sample_size.val;
+	double Sd = sample_std.val;
+	double T = 0;
+
+	// Rounding errors occur past this point. Since the T distribution converges toward the normal distribution as N grows large,
+	// we can switch to using the normal distribution without much of a difference in the statistic.
+	if ( Sn > 1404454273 ) {
+		normal dist(0.0,1.0);
+		if( two_tailed.val ) {
+			T = quantile( complement(dist, (1-confidence.val)/2.0 ) );
+		} else {
+			T = quantile( dist, confidence.val );
+		}
+	} else {
+		students_t dist(Sn - 1);
+		if( two_tailed.val ) {
+			T = quantile( complement(dist, (1-confidence.val)/2.0 ) );
+		} else {
+			T = quantile( dist, confidence.val );
+		}
+	}
+
+	return DoubleVal( T * Sd / sqrt( double(Sn) ) );
+}
+
+
+// COnfidence interval function, but we hard-code the two-sided case for function overloading.
+IMPALA_UDF_EXPORT
+DoubleVal ci_t_twoSided(FunctionContext* context, const DoubleVal& confidence, const BigIntVal& sample_size, const DoubleVal& sample_std ) {
+	return ci_t(context,confidence,sample_size,sample_std,BooleanVal(true));
+}
---
+++ udf-mathutils.h
@@ -0,0 +1,32 @@
+// Copyright 2012 Cloudera Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Altered by Sam Shepard for use with udf-mathutils
+// 2017-07-08
+
+#ifndef UDF_BIOUTILS_H
+#define UDF_BIOUTILS_H
+
+#include <impala_udf/udf.h>
+#include <string>
+#include <vector>
+
+using namespace impala_udf;
+
+// private functions
+DoubleVal 	qt(FunctionContext* context, const DoubleVal& confidence, const BigIntVal& sample_size, const BooleanVal& two_tailed );
+DoubleVal	ci_t(FunctionContext* context, const DoubleVal& confidence, const BigIntVal& sample_size, const DoubleVal& sample_std, const BooleanVal& two_tailed );
+DoubleVal	ci_t_twoSided(FunctionContext* context, const DoubleVal& confidence, const BigIntVal& sample_size, const DoubleVal& sample_std );
+
+#endif

72eb099eccf35beac6a6166416f6dd0edd238105
Samuel Shepard <vfn4@cdc.gov>
2020-11-16 09:57:23 -0500

    Added install/upload script to aid in deployment.

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [[ "${1,,}" =~ "dev" ]];then
+	deployment=dev
+else
+	deployment=prod
+fi
+
+output_path=/udx/ncird_id/${deployment}
+
+
+SO=(
+	build/libudabioutils.so
+	build/libudfbioutils.so
+	build/libudfmathutils.so
+	build/libudf-sero.so
+)
+
+hdfs dfs -ls $output_path
+for s in "${SO[@]}";do
+	echo "Depositing $s"
+	hdfs dfs -put -f $s $output_path/$(basename $s)
+done
+hdfs dfs -ls $output_path
